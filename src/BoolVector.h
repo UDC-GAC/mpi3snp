2a0158f9f2ec845686a49c60117d2a285b074f67
Christian Ponte Fernández <christian.ponte@udc.es>
2017-01-24 23:58:18 +0100

    BoolVector: added new class

---
+++ src/BoolVector.h
@@ -0,0 +1,29 @@
+//
+// Created by christian on 23/01/17.
+//
+
+#ifndef BOOLVECTOR_H
+#define BOOLVECTOR_H
+
+#include <cstdlib>
+
+class BoolVector{
+public:
+    BoolVector();
+    ~BoolVector();
+    size_t trueCount();
+    size_t falseCount();
+    void push_back(bool value);
+    bool get(size_t pos);
+    size_t size();
+    void resize(size_t newSize);
+    bool &operator [](size_t pos){
+        return array[pos];
+    }
+private:
+    static const int INITIAL_SIZE = 1024;
+    bool *array;
+    size_t arraySize, count, tCount, fCount;
+};
+
+#endif //BOOLVECTOR_H
