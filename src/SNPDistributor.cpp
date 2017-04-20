1ea95e5e320602e98f61879f00d7d4126878f0f1
Christian Ponte <christian.ponte@udc.es>
2017-04-20 13:35:20 +0200

    SNPDistributor: replaced manual bool vector management with class BoolVector

--- src/SNPDistributor.cpp
+++ src/SNPDistributor.cpp
@@ -7,209 +7,192 @@
 
 #include "SNPDistributor.h"
 
-SNPDistributor::SNPDistributor(Options* options) {
-	_options = options;
-	_snpSet.resize(DEFAULT_NUM_SNPS);
+SNPDistributor::SNPDistributor(Options *options) {
+    _options = options;
+    _snpSet.resize(DEFAULT_NUM_SNPS);
+	bv.reserve(DEFAULT_NUM_INDS);
 
-	_withLock = (_options->getNumCPUs()) > 1;
-	pthread_mutex_init(&_mutex, NULL);
+    _withLock = (_options->getNumCPUs()) > 1;
+    pthread_mutex_init(&_mutex, NULL);
 
-	if((_fpOut = myfopen(options->getOutFileName().c_str(), "wb")) == NULL){
-		Utils::exit("Out file: file %s could not be opened\n", options->getOutFileName().c_str());
-	}
+    if ((_fpOut = myfopen(options->getOutFileName().c_str(), "wb")) == NULL) {
+        Utils::exit("Out file: file %s could not be opened\n", options->getOutFileName().c_str());
+    }
 
-	if((_fpTfam = myfopen(options->getTFAMFileName().c_str(), "r")) == NULL){
-		Utils::exit("TFAM file: file %s could not be opened\n", options->getTFAMFileName().c_str());
-	}
+    if ((_fpTfam = myfopen(options->getTFAMFileName().c_str(), "r")) == NULL) {
+        Utils::exit("TFAM file: file %s could not be opened\n", options->getTFAMFileName().c_str());
+    }
 
-	if((_fpTped = myfopen(options->getTPEDFileName().c_str(), "r")) == NULL){
-		Utils::exit("TPED file: file %s could not be opened\n", options->getTPEDFileName().c_str());
-	}
+    if ((_fpTped = myfopen(options->getTPEDFileName().c_str(), "r")) == NULL) {
+        Utils::exit("TPED file: file %s could not be opened\n", options->getTPEDFileName().c_str());
+    }
 
-	_indsClass = new bool[DEFAULT_NUM_INDS];
-	_boolArrSize = DEFAULT_NUM_INDS;
-	_lineReader = new LineReader();
+    _lineReader = new LineReader();
 
-	_numSnp = 0;
-	_numCases = 0;
-	_numCtrls = 0;
+    _numSnp = 0;
 
-	_moreDouble = true;
+    _moreDouble = true;
 
-	_iterDoubleSnp1 = 0;
-	_iterDoubleSnp2 = 1;
+    _iterDoubleSnp1 = 0;
+    _iterDoubleSnp2 = 1;
 }
 
 SNPDistributor::~SNPDistributor() {
 
-	myfclose(_fpTfam);
-	myfclose(_fpTped);
-	myfclose(_fpOut);
+    myfclose(_fpTfam);
+    myfclose(_fpTped);
+    myfclose(_fpOut);
 }
 
-void SNPDistributor::_loadIndsClass(){
-	// Load the information from a TFAM file about the cases and controls
-	int numInds = 0;
-	int retValue;
-	_numCases = 0;
-	_numCtrls = 0;
+void SNPDistributor::_loadIndsClass() {
+    // Load the information from a TFAM file about the cases and controls
+    int numInds = 0;
+    int retValue;
 
-	while((retValue = _lineReader->readTFAMLine(_fpTfam, numInds)) >= 0){
-		if(numInds >= _boolArrSize){
-			_resizeBoolArr(_boolArrSize);
-		}
-		if(retValue){
-			_numCtrls++;
-			_indsClass[numInds] = true;
-		}
-		else{
-			_numCases++;
-			_indsClass[numInds] = false;
-		}
-		numInds++;
-	}
+    while ((retValue = _lineReader->readTFAMLine(_fpTfam, numInds)) >= 0) {
+        bv.push_back(retValue);
+    }
 
-	Utils::log("Loaded information of %ld individuals (%ld/%ld cases/controls)\n", numInds, _numCases, _numCtrls);
+
+    printf("Loaded information of %ld individuals (%ld/%ld cases/controls)\n", numInds, getNumCases(), getNumCtrls());
 #ifdef DEBUG
-	for(int i=0; i<numInds; i++){
-		if(_indsClass[i]){
-			Utils::log("control ");
-		}
-		else{
-			Utils::log("case ");
-		}
-	}
-	Utils::log("\n");
+    for(int i=0; i<numInds; i++){
+        if(_indsClass[i]){
+            Utils::log("control ");
+        }
+        else{
+            Utils::log("case ");
+        }
+    }
+    Utils::log("\n");
 #endif
 }
 
-void SNPDistributor::loadSNPSet(){
+void SNPDistributor::loadSNPSet() {
 
-	// Load the information of which individuals are cases and controls
-	_loadIndsClass();
+    // Load the information of which individuals are cases and controls
+    _loadIndsClass();
 
-	SNP* readSNP;
+    SNP *readSNP;
 
-	_numSnp = 0;
-	while(1){
-		readSNP = new(SNP);
+    _numSnp = 0;
+    while (1) {
+        readSNP = new(SNP);
 
-		if(!_lineReader->readTPEDLine(_fpTped, readSNP, _numSnp, _numCases+_numCtrls, _indsClass)){
-			delete readSNP;
-			break;
-		}
+        if (!_lineReader->readTPEDLine(_fpTped, readSNP, _numSnp, bv.size(), &bv[0])) {
+            delete readSNP;
+            break;
+        }
 
-		if(_snpSet.size() <= _numSnp){
-			_snpSet.resize(_snpSet.size()+DEFAULT_NUM_SNPS);
-		}
-		_snpSet[_numSnp] = readSNP;
-		_numSnp++;
-	}
+        if (_snpSet.size() <= _numSnp) {
+            _snpSet.resize(_snpSet.size() + DEFAULT_NUM_SNPS);
+        }
+        _snpSet[_numSnp] = readSNP;
+        _numSnp++;
+    }
 
-	if(!_numSnp){
-		_moreDouble = false;
-	}
+    if (!_numSnp) {
+        _moreDouble = false;
+    }
 
 #ifdef DEBUG
-	int j;
-	uint16_t casesAa, ctrlsAa;
-	for(int i=0; i<_numSnp; i++){
-		Utils::log("SNP %d:\n   name %s\n   cases 0", i, (char *)_snpSet[i]->_name);
-		for(j=0; j<(_numCases/32+((_numCases%32)>0)); j++){
-			Utils::log("%u ", _snpSet[i]->_case0Values[j]);
-		}
+    int j;
+    uint16_t casesAa, ctrlsAa;
+    for(int i=0; i<_numSnp; i++){
+        Utils::log("SNP %d:\n   name %s\n   cases 0", i, (char *)_snpSet[i]->_name);
+        for(j=0; j<(_numCases/32+((_numCases%32)>0)); j++){
+            Utils::log("%u ", _snpSet[i]->_case0Values[j]);
+        }
 
-		Utils::log("\n   cases 2 ");
-		for(j=0; j<(_numCases/32+((_numCases%32)>0)); j++){
-			Utils::log("%u ", _snpSet[i]->_case2Values[j]);
-		}
-		Utils::log("\n   controls 0 ");
-		for(j=0; j<(_numCtrls/32+((_numCtrls%32)>0)); j++){
-			Utils::log("%u ", _snpSet[i]->_ctrl0Values[j]);
-		}
+        Utils::log("\n   cases 2 ");
+        for(j=0; j<(_numCases/32+((_numCases%32)>0)); j++){
+            Utils::log("%u ", _snpSet[i]->_case2Values[j]);
+        }
+        Utils::log("\n   controls 0 ");
+        for(j=0; j<(_numCtrls/32+((_numCtrls%32)>0)); j++){
+            Utils::log("%u ", _snpSet[i]->_ctrl0Values[j]);
+        }
 
-		Utils::log("\n   controls 2 ");
-		for(j=0; j<(_numCtrls/32+((_numCtrls%32)>0)); j++){
-			Utils::log("%u ", _snpSet[i]->_ctrl2Values[j]);
-		}
-	}
+        Utils::log("\n   controls 2 ");
+        for(j=0; j<(_numCtrls/32+((_numCtrls%32)>0)); j++){
+            Utils::log("%u ", _snpSet[i]->_ctrl2Values[j]);
+        }
+    }
 #endif
 }
 
-uint32_t SNPDistributor::_getPairsSNPsNoLock(uint32_t* ids){
+uint32_t SNPDistributor::_getPairsSNPsNoLock(uint32_t *ids) {
 
-	uint32_t id1;
-	uint32_t id2;
+    uint32_t id1;
+    uint32_t id2;
 
-	if(_moreDouble){
-		uint16_t iter_block = 0;
-		while(iter_block < NUM_PAIRS_BLOCK){
+    if (_moreDouble) {
+        uint16_t iter_block = 0;
+        while (iter_block < NUM_PAIRS_BLOCK) {
 
-			id1 = _iterDoubleSnp1;
-			id2 = _iterDoubleSnp2;
+            id1 = _iterDoubleSnp1;
+            id2 = _iterDoubleSnp2;
 
-			ids[2*iter_block] = id1;
-			ids[2*iter_block+1] = id2;
+            ids[2 * iter_block] = id1;
+            ids[2 * iter_block + 1] = id2;
 
-			iter_block++;
+            iter_block++;
 
-			// Look for the next pair
-			if(_iterDoubleSnp2 == _numSnp-1){
-				_iterDoubleSnp1++;
-				_iterDoubleSnp2 = _iterDoubleSnp1+1;
-			}
-			else{
-				_iterDoubleSnp2++;
-			}
+            // Look for the next pair
+            if (_iterDoubleSnp2 == _numSnp - 1) {
+                _iterDoubleSnp1++;
+                _iterDoubleSnp2 = _iterDoubleSnp1 + 1;
+            } else {
+                _iterDoubleSnp2++;
+            }
 
-			if(_iterDoubleSnp1 == _numSnp-1){ // We have finished to compute the block
-				_moreDouble = false;
-				break;
-			}
-		}
+            if (_iterDoubleSnp1 == _numSnp - 1) { // We have finished to compute the block
+                _moreDouble = false;
+                break;
+            }
+        }
 
-		return iter_block;
-	}
-	return 0;
+        return iter_block;
+    }
+    return 0;
 }
 
-uint32_t SNPDistributor::_getPairsSNPsLock(uint32_t* ids){
+uint32_t SNPDistributor::_getPairsSNPsLock(uint32_t *ids) {
 
-	uint32_t id1;
-	uint32_t id2;
+    uint32_t id1;
+    uint32_t id2;
 
-	_lock();
+    _lock();
 
-	if(_moreDouble){
-		uint16_t iter_block = 0;
-		while(iter_block < NUM_PAIRS_BLOCK){
+    if (_moreDouble) {
+        uint16_t iter_block = 0;
+        while (iter_block < NUM_PAIRS_BLOCK) {
 
-			id1 = _iterDoubleSnp1;
-			id2 = _iterDoubleSnp2;
+            id1 = _iterDoubleSnp1;
+            id2 = _iterDoubleSnp2;
 
-			ids[2*iter_block] = id1;
-			ids[2*iter_block+1] = id2;
+            ids[2 * iter_block] = id1;
+            ids[2 * iter_block + 1] = id2;
 
-			iter_block++;
+            iter_block++;
 
-			// Look for the next pair
-			if(_iterDoubleSnp2 == _numSnp-1){
-				_iterDoubleSnp1++;
-				_iterDoubleSnp2 = _iterDoubleSnp1+1;
-			}
-			else{
-				_iterDoubleSnp2++;
-			}
+            // Look for the next pair
+            if (_iterDoubleSnp2 == _numSnp - 1) {
+                _iterDoubleSnp1++;
+                _iterDoubleSnp2 = _iterDoubleSnp1 + 1;
+            } else {
+                _iterDoubleSnp2++;
+            }
 
-			if(_iterDoubleSnp1 == _numSnp-1){ // We have finished to compute the block
-				_moreDouble = false;
-				break;
-			}
-		}
+            if (_iterDoubleSnp1 == _numSnp - 1) { // We have finished to compute the block
+                _moreDouble = false;
+                break;
+            }
+        }
 
-		_unlock();
-		return iter_block;
-	}
-	_unlock();
-	return 0;
+        _unlock();
+        return iter_block;
+    }
+    _unlock();
+    return 0;
 }
--- src/SNPDistributor.h
+++ src/SNPDistributor.h
@@ -13,115 +13,96 @@
 #include "MyFile.h"
 #include "LineReader.h"
 #include "MutualInfo.h"
+#include "BoolVector.h"
 
 class SNPDistributor {
 public:
-	SNPDistributor(Options* options);
-	virtual ~SNPDistributor();
+    SNPDistributor(Options *options);
 
-	// Loads all the SNPs in the distributor
-	void loadSNPSet();
+    virtual ~SNPDistributor();
 
-	// Returns the number of pairs that will be computed (all the 3-way combinations)
-	// Also return the ids of the pairs
-	inline uint32_t getPairsSNPs(uint32_t* ids){
-		if(_withLock){
-			return _getPairsSNPsLock(ids);
-		}
-		else{
-			return _getPairsSNPsNoLock(ids);
-		}
-	}
+    // Loads all the SNPs in the distributor
+    void loadSNPSet();
 
-	inline void printMI(MutualInfo* info, uint16_t numOutputs){
-		for(int i=numOutputs-1; i>=0; i--){
-			MutualInfo auxInfo = info[i];
-			_lineReader->printMI(_fpOut, auxInfo._id1, auxInfo._id2, auxInfo._id3, auxInfo._mutualInfoValue);
-		}
-	}
+    // Returns the number of pairs that will be computed (all the 3-way combinations)
+    // Also return the ids of the pairs
+    inline uint32_t getPairsSNPs(uint32_t *ids) {
+        if (_withLock) {
+            return _getPairsSNPsLock(ids);
+        } else {
+            return _getPairsSNPsNoLock(ids);
+        }
+    }
 
-	inline uint32_t getNumSnp(){
-		return _numSnp;
-	}
+    inline void printMI(MutualInfo *info, uint16_t numOutputs) {
+        for (int i = numOutputs - 1; i >= 0; i--) {
+            MutualInfo auxInfo = info[i];
+            _lineReader->printMI(_fpOut, auxInfo._id1, auxInfo._id2, auxInfo._id3, auxInfo._mutualInfoValue);
+        }
+    }
 
-	inline uint16_t getNumCases(){
-		return _numCases;
-	}
+    inline uint32_t getNumSnp() {
+        return _numSnp;
+    }
 
-	inline uint16_t getNumCtrls(){
-		return _numCtrls;
-	}
+    inline uint16_t getNumCases() {
+        return bv.falseCount();
+    }
 
-	inline vector<SNP*> getSnpSet(){
-		return _snpSet;
-	}
+    inline uint16_t getNumCtrls() {
+        return bv.trueCount();
+    }
+
+    inline vector<SNP *> getSnpSet() {
+        return _snpSet;
+    }
 
 private:
-	// Load the information of which individuals are control and case from the tfam file
-	void _loadIndsClass();
-	inline void _resizeBoolArr(size_t nsize){
-		if(nsize< _boolArrSize){
-			return;
-		}
+    // Load the information of which individuals are control and case from the tfam file
+    void _loadIndsClass();
 
-		// Allocate a new buffer
-		_boolArrSize = nsize*2;
-		bool* nbuffer = new bool[_boolArrSize];
-		if (!nbuffer) {
-			Utils::exit("Memory reallocation failed in file %s in line %d\n",
-					__FUNCTION__, __LINE__);
-		}
-		// Copy the old data
-		memcpy(nbuffer, _indsClass, (_boolArrSize/2)*sizeof(bool));
+    // Functions to work with mutex
+    inline void _lock() {
+        if (_withLock) {
+            pthread_mutex_lock(&_mutex);
+        }
+    }
 
-		// Release the old buffer
-		delete[] _indsClass;
-		_indsClass = nbuffer;
-	}
+    inline void _unlock() {
+        if (_withLock) {
+            pthread_mutex_unlock(&_mutex);
+        }
+    }
 
-	// Functions to work with mutex
-	inline void _lock() {
-		if (_withLock) {
-			pthread_mutex_lock(&_mutex);
-		}
-	}
-	inline void _unlock() {
-		if (_withLock) {
-			pthread_mutex_unlock(&_mutex);
-		}
-	}
+    uint32_t _getPairsSNPsLock(uint32_t *ids);
 
-	uint32_t _getPairsSNPsLock(uint32_t* ids);
-	uint32_t _getPairsSNPsNoLock(uint32_t* ids);
+    uint32_t _getPairsSNPsNoLock(uint32_t *ids);
 
-	Options* _options;
-	vector<SNP*> _snpSet;
-	// File handler
-	MyFilePt _fpTfam;
-	MyFilePt _fpTped;
-	MyFilePt _fpOut;
-	LineReader* _lineReader;
-	bool _withLock;
+    Options *_options;
+    vector<SNP *> _snpSet;
+    // File handler
+    MyFilePt _fpTfam;
+    MyFilePt _fpTped;
+    MyFilePt _fpOut;
+    LineReader *_lineReader;
+    bool _withLock;
 
-	// Variables to indicate if there are more analyses to perform
-	bool _moreDouble;
+    // Variables to indicate if there are more analyses to perform
+    bool _moreDouble;
 
-	// Variables shared among all threads
-	pthread_mutex_t _mutex;
+    // Variables shared among all threads
+    pthread_mutex_t _mutex;
 
-	// Array to keep which values are cases
-	// False cases, true controls
-	bool* _indsClass;
-	size_t _boolArrSize;
+    // Array to keep which values are cases
+    // False cases, true controls
+    BoolVector bv;
 
-	// To store the dimensions of the problem
-	uint32_t _numSnp;
-	uint16_t _numCases;
-	uint16_t _numCtrls;
+    // To store the dimensions of the problem
+    uint32_t _numSnp;
 
-	// Iterators for the SNPs
-	uint32_t _iterDoubleSnp1;
-	uint32_t _iterDoubleSnp2;
+    // Iterators for the SNPs
+    uint32_t _iterDoubleSnp1;
+    uint32_t _iterDoubleSnp2;
 };
 
 #endif /* SNPDISTRIBUTOR_H_ */
