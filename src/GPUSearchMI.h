42ba1031c3a061f4ceabf3d055c677d355eb78d5
Christian Ponte <christian.ponte@udc.es>
2017-06-06 15:41:16 +0200

    GPUSearchMI: replaced previous class implementation with single call to GPUEngine

--- src/GPUSearchMI.h
+++ src/GPUSearchMI.h
@@ -9,7 +9,6 @@
 #define GPUSEARCHMI_H_
 
 #include "Search.h"
-#include "ThreadParams.h"
 
 class GPUSearchMI : public Search {
 public:
@@ -18,14 +17,6 @@ public:
 
 	// Execute the epistasis search
 	void execute();
-
-private:
-	static void* _GPUMI(void* arg);
-
-	vector<GPUEngine*> _engines;
-	vector<ThreadParams*> _threadParams;
-
-	bool _dynamicDist;
 };
 
 #endif /* GPUSEARCHMI_H_ */
