46d1dcf7164f932836199617cc534c6c0c7f593a
Christian Ponte <christian.ponte@udc.es>
2017-06-06 11:36:42 +0200

    Created class EntropySearch from old GPUEngine implementation

---
+++ src/EntropySearch.h
@@ -0,0 +1,77 @@
+//
+// Created by christian on 06/06/17.
+//
+
+#ifndef MPI3SNP_ENTROPYSEARCH_H
+#define MPI3SNP_ENTROPYSEARCH_H
+
+#include "MutualInfo.h"
+#include "GPUMacros.h"
+#include "GPUContTable.h"
+
+class EntropySearch {
+    EntropySearch(bool isMI, uint32_t numSNPs, uint16_t numCases, uint16_t numCtrls, uint16_t numOutputs,
+                  uint32_t *host0Cases, uint32_t *host1Cases, uint32_t *host2Cases, uint32_t *host0Ctrls,
+                  uint32_t *host1Ctrls, uint32_t *host2Ctrls);
+
+    ~EntropySearch();
+
+    void mutualInfo(uint64_t numPairs, uint2 *ids, MutualInfo *mutualInfo, float &minMI, uint16_t &minMIPos,
+                    uint16_t &numEntriesWithMI);
+
+private:
+    bool _isMI;
+    uint16_t _numOutputs;
+
+    uint32_t _numSNPs;
+    uint16_t _numCases;
+    uint16_t _numCtrls;
+    uint16_t _numEntriesCase;
+    uint16_t _numEntriesCtrl;
+
+    uint32_t *_dev0Cases;
+    uint32_t *_dev1Cases;
+    uint32_t *_dev2Cases;
+    uint32_t *_dev0Ctrls;
+    uint32_t *_dev1Ctrls;
+    uint32_t *_dev2Ctrls;
+    uint2 *_devIds;
+
+    // Auxiliary array for the contingency tables between the two kernels
+    GPUDoubleContTable *_devDoubleTables;
+    GPUDoubleContTable *_tables;
+
+    // Auxiliary array to store the MI values of each block
+    float *_devMIValues;
+    float *_hostMIValues;
+
+    // Auxiliary arrays to store the ids that are in the list of MIs
+    uint3 *_devMiIds;
+    uint3 *_hostMiIds;
+
+    static __global__ void _kernelDoubleTable(uint64_t numPairs, uint32_t numSNPs,
+                                              uint16_t numEntriesCases, uint16_t numEntriesCtrls, uint2 *devIds,
+                                              uint32_t *dev0Cases, uint32_t *dev1Cases, uint32_t *dev2Cases,
+                                              uint32_t *dev0Ctrls, uint32_t *dev1Ctrls, uint32_t *dev2Ctrls,
+                                              GPUDoubleContTable *doubleTables);
+
+    static __global__ void _kernelTripleMI(uint64_t numPairs, uint32_t numSNPs,
+                                           uint16_t numEntriesCases, uint16_t numEntriesCtrls, uint2 *devIds,
+                                           uint32_t *dev0Cases, uint32_t *dev1Cases, uint32_t *dev2Cases,
+                                           uint32_t *dev0Ctrls, uint32_t *dev1Ctrls, uint32_t *dev2Ctrls,
+                                           GPUDoubleContTable *devDoubleTables, uint16_t numOutputs,
+                                           float *devMIValues, uint3 *devMiIds);
+
+    static __global__ void _kernelTripleIG(uint64_t numPairs, uint32_t numSNPs,
+                                           uint16_t numEntriesCases, uint16_t numEntriesCtrls, uint2 *devIds,
+                                           uint32_t *dev0Cases, uint32_t *dev1Cases, uint32_t *dev2Cases,
+                                           uint32_t *dev0Ctrls, uint32_t *dev1Ctrls, uint32_t *dev2Ctrls,
+                                           GPUDoubleContTable *devDoubleTables, uint16_t numOutputs,
+                                           float *devMIValues, uint3 *devMiIds);
+
+    void _findNHighestMI(MutualInfo *mutualInfo, uint64_t totalValues, float &minMI, uint16_t &minMIPos,
+                         uint16_t &numEntriesWithMI);
+};
+
+
+#endif //MPI3SNP_ENTROPYSEARCH_H
