9cbffd79db009e17a11d9431901418c4c7957d5f
Christian Ponte <christian.ponte@udc.es>
2017-06-06 18:59:34 +0200

    Imported IOMpi from cpu_dev branch

---
+++ src/IOMpi.cpp
@@ -0,0 +1,118 @@
+//
+// Created by christian on 10/03/17.
+//
+
+#include "IOMpi.h"
+
+IOMpi::IOMpi() {
+    MPI_Comm_dup(MPI_COMM_WORLD, &io_comm);
+    MPI_Comm_size(io_comm, &comm_size);
+    MPI_Comm_rank(io_comm, &my_rank);
+    io_rank = MPI_PROC_NULL;
+    cprintf_tag = 0;
+    pthread_mutex_init(&cprintf_mutex, NULL);
+    io_buff = NULL;
+}
+
+IOMpi::~IOMpi() {
+    pthread_mutex_destroy(&cprintf_mutex);
+    if (io_buff != NULL){
+        delete[] io_buff;
+    }
+}
+
+int IOMpi::Get_io_rank() {
+    // Determine IO process
+    int mpi_io, flag, io_rank;
+
+    MPI_Attr_get(MPI_COMM_WORLD, MPI_IO, &mpi_io, &flag);
+
+    if (!flag) {
+        // Attribute not cached
+        io_rank = 0;
+    } else if (mpi_io == MPI_PROC_NULL || mpi_io == MPI_ANY_SOURCE || comm_size < mpi_io) {
+        io_rank = 0;
+    } else {
+        // Multiple IO processes
+        int err = MPI_Allreduce(&mpi_io, &io_rank, 1, MPI_INT, MPI_MIN, io_comm);
+        if (err != MPI_SUCCESS){
+            int error_class, len;
+            char error_string[1024];
+            MPI_Error_class(err, &error_class);
+            MPI_Error_string(error_class, error_string, &len);
+            printf(error_string);
+            io_rank = 0;
+        }
+    }
+
+    return io_rank;
+}
+
+// Collective printf
+int IOMpi::Cprintf(const char *format, ...) {
+    va_list args;
+    int tag, i;
+
+    if (io_rank == MPI_PROC_NULL){
+        io_rank = Get_io_rank();
+    }
+
+    if (io_buff == NULL){
+        io_buff = new char[BUFFER_SIZE];
+    }
+
+    pthread_mutex_lock(&cprintf_mutex);
+    tag = cprintf_tag++;
+    pthread_mutex_unlock(&cprintf_mutex);
+
+    if (my_rank == io_rank){
+        for (i=0; i<my_rank; i++){
+            if (MPI_Recv(io_buff, BUFFER_SIZE, MPI_CHAR, i, tag, io_comm, NULL) != MPI_SUCCESS){
+                return -1;
+            }
+            printf("Process %d > %s", i, io_buff);
+            fflush(stdout);
+        }
+        va_start(args, format);
+        vsprintf(io_buff, format, args);
+        va_end(args);
+        printf("Process %d > %s", my_rank, io_buff);
+        for (i=my_rank+1; i<comm_size; i++){
+            if (MPI_Recv(io_buff, BUFFER_SIZE, MPI_CHAR, i, tag, io_comm, NULL) != MPI_SUCCESS){
+                return -1;
+            }
+            printf("Process %d > %s", i, io_buff);
+            fflush(stdout);
+        }
+    } else {
+        va_start(args, format);
+        vsprintf(io_buff, format, args);
+        va_end(args);
+        if (MPI_Send(io_buff, strlen(io_buff) + 1, MPI_CHAR, io_rank, tag, io_comm) != MPI_SUCCESS){
+            return -1;
+        }
+    }
+    /* TODO:
+     - return number of characters printed
+    */
+    return 0;
+}
+
+int IOMpi::Mprintf(const char *format, ...) {
+    if (my_rank != 0){
+        return 0;
+    }
+
+    if (io_buff == NULL){
+        io_buff = new char[BUFFER_SIZE];
+    }
+
+    va_list args;
+    va_start(args, format);
+    vsprintf(io_buff, format, args);
+    va_end(args);
+    printf(io_buff);
+    fflush(stdout);
+
+    return 0;
+}
---
+++ src/IOMpi.h
@@ -0,0 +1,59 @@
+//
+// Created by christian on 10/03/17.
+//
+
+#ifndef MPI3SNP_IOMpi_H
+#define MPI3SNP_IOMpi_H
+
+#include <mpi.h>
+
+/*
+ * CLASS: IOMpi
+ *
+ * DESCRIPTION: Handles all the I/O throughout the program. Uses Meyers' Singleton pattern, which describes a thread-safe
+ *  singleton initialization (C++11 or superior). The object destruction is handled at the end of the program; however,
+ *  as MPI dependant resources need to be freed before calling MPI_Finalize, some attributes are destroyed with the call
+ *  Deallocate_MPI_resources().
+ */
+
+class IOMpi {
+public:
+    static IOMpi &Instance() {
+        static IOMpi inst;
+        return inst;
+    }
+
+    static void Deallocate_MPI_resources(){
+        MPI_Comm_free(&IOMpi::Instance().io_comm);
+    }
+
+    // delete copy and move constructors and assign operators
+    IOMpi(IOMpi const &) = delete;             // Copy construct
+    IOMpi(IOMpi &&) = delete;                  // Move construct
+    IOMpi &operator=(IOMpi const &) = delete;  // Copy assign
+    IOMpi &operator=(IOMpi &&) = delete;      // Move assign
+
+    int Cprintf(const char *format, ...);
+
+    int Mprintf(const char *format, ...);
+
+protected:
+    /* Methods */
+    IOMpi();
+
+    ~IOMpi();
+
+    int Get_io_rank();
+
+    /* Attributes */
+    const int BUFFER_SIZE = 1024;
+
+    MPI_Comm io_comm;
+    int io_rank, my_rank, comm_size;
+    int cprintf_tag;
+    pthread_mutex_t cprintf_mutex;
+    char *io_buff;
+};
+
+
+#endif //MPI3SNP_IOMpi_H
