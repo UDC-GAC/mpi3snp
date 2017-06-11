93cdfea9cdbe50b0c679e3ddfb11a6030971f35e
Christian Ponte <christian.ponte@udc.es>
2017-06-12 01:23:48 +0200

    Added Definitions header

---
+++ src/Definitions.h
@@ -0,0 +1,19 @@
+//
+// Created by christian on 11/06/17.
+//
+
+#ifndef MPI3SNP_DEFINITIONS_H
+#define MPI3SNP_DEFINITIONS_H
+
+#define MPI3SNP_NAME "@PROJECT_NAME@"
+#define MPI3SNP_VERSION "@PROJECT_VERSION_MAJOR@.@PROJECT_VERSION_MINOR@"
+#define MPI3SNP_LICENSE "Copyright (C) 2017 Christian Ponte.\n\
+License GPLv3: GNU GPL version 3 <http://gnu.org/licenses/gpl.html>.\n\
+This is free software, and you are welcome to redistribute it under certain conditions.\n\
+This program comes with ABSOLUTELY NO WARRANTY."
+#define MPI3SNP_AUTHOR "Written by Christian Ponte. Special thanks to Jorge González and María J. Martín."
+#define MPI3SNP_DESCRIPTION "Description"
+#define MPI3SNP_HELP "@PROJECT_NAME@ online help: <https://github.com/christianponte/mpi3snp/>\n\
+   Documentation available at: <https://github.com/christianponte/mpi3snp/wiki>"
+
+#endif //MPI3SNP_DEFINITIONS_H
