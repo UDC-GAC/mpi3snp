ecedbe14852dec9d2fd55b5a0a55d8cfa3636222
Christian Ponte <christian.ponte@udc.es>
2017-06-16 01:06:04 +0200

    Added new SNP struct

---
+++ src/AltSNP.h
@@ -0,0 +1,85 @@
+//
+// Created by christian on 12/06/17.
+//
+
+#ifndef MPI3SNP_SNP2_H_H
+#define MPI3SNP_SNP2_H_H
+
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+struct AltSNP {
+    class InvalidSNP : public std::runtime_error {
+    public:
+        InvalidSNP(const std::string &message) : std::runtime_error(message) {};
+
+        virtual ~InvalidSNP() {};
+    };
+
+    std::string chr_code; // Chromosome code
+    std::string v_id; // Variant identifier
+    double pos; // Position in morgans or centimorgans
+    unsigned int coord; // Base-pair coordinate
+    std::vector<uint8_t> genotypes;
+
+    friend std::istream &operator>>(std::istream &str, AltSNP &snp) {
+        std::string line;
+        std::vector<char> nucleotides;
+        AltSNP tmp;
+        if (std::getline(str, line)) {
+            std::stringstream iss(line);
+            // Parse SNP information
+            if (!(iss >> tmp.chr_code &&
+                  iss >> tmp.v_id &&
+                  iss >> tmp.pos &&
+                  iss >> tmp.coord)) {
+                throw InvalidSNP("invalid loci information");
+            }
+
+            // Read nucleotides
+            char c;
+            while (iss >> c && (c == 'A' || c == 'C' || c == 'T' || c == 'G')) {
+                nucleotides.push_back(c);
+            }
+
+            if (!iss.eof()) {
+                throw InvalidSNP(std::string("invalid nucleotide value '") + c + "' at position " +
+                                 std::to_string(iss.tellg()));
+            }
+
+            if (nucleotides.size() % 2) {
+                throw InvalidSNP("odd number of nucleotides");
+            }
+
+            // Find minor character
+            char a1 = nucleotides[0];
+            auto pos = std::find_if(nucleotides.begin(), nucleotides.end(), [&a1](char n) { return n != a1; });
+            char a2 = pos == nucleotides.end() ? a1 : *pos;
+            a1 = std::min(a1, a2);
+
+            // Translate nucleotides into genotype
+            tmp.genotypes.reserve(nucleotides.size() / 2);
+            uint8_t allele;
+            for (auto it = nucleotides.begin(); it < nucleotides.end();) {
+                allele = (*(it++) != a1) + (*(it++) != a1);
+                tmp.genotypes.push_back(allele);
+            }
+
+            snp.swap(tmp);
+        }
+        return str;
+    }
+
+    void swap(AltSNP &other) {
+        std::swap(chr_code, other.chr_code);
+        std::swap(v_id, other.v_id);
+        std::swap(pos, other.pos);
+        std::swap(coord, other.coord);
+        std::swap(genotypes, other.genotypes);
+    }
+};
+
+#endif //MPI3SNP_SNP2_H_H
