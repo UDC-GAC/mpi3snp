8879c748aebf6726e43ffe0ff605df4d54bd47fc
Christian Ponte <christian.ponte@udc.es>
2017-06-17 15:50:56 +0200

    Distributor: removed Macros usage

--- src/Distributor.h
+++ src/Distributor.h
@@ -5,19 +5,20 @@
 #ifndef MPI3SNP_DISTRIBUTOR_H
 #define MPI3SNP_DISTRIBUTOR_H
 
-
-#include <cstdint>
 #include <cuda_runtime.h>
-#include "Macros.h"
+#include <inttypes.h>
+#include <pthread.h>
 
 class Distributor {
 public:
     Distributor(unsigned int proc_num, unsigned int proc_id, unsigned long item_num, bool shared,
-                unsigned long block_size = NUM_PAIRS_BLOCK);
+                unsigned long block_size = DEFAULT_PAIRS_BLOCK);
 
     ~Distributor();
 
     unsigned long Get_pairs(uint2 *values, uint64_t &totalAnal);
+
+    static const unsigned long DEFAULT_PAIRS_BLOCK = 25000;
 
 private:
     unsigned long Get_pairs_lock(uint2 *values, uint64_t &totalAnal);
