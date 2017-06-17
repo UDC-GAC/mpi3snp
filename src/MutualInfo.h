6fda8b08b4c688abfcda7d5f4302b4c8ba23d072
Christian Ponte <christian.ponte@udc.es>
2017-06-17 15:51:02 +0200

    MutualInfo: removed Macros usage

--- src/MutualInfo.h
+++ src/MutualInfo.h
@@ -8,28 +8,26 @@
 #ifndef MUTUALINFO_H_
 #define MUTUALINFO_H_
 
-#include "Macros.h"
+#include <inttypes.h>
 
 /*
  * Structure for the outputs with the result of the mutual informations and the SNPs associated
  */
 
-struct MutualInfo
-{
-	MutualInfo(){
-		_id1 = 0;
-		_id2 = 0;
-		_id3 = 0;
-		_mutualInfoValue = 0.0;
-	}
+struct MutualInfo {
+    MutualInfo() {
+        _id1 = 0;
+        _id2 = 0;
+        _id3 = 0;
+        _mutualInfoValue = 0.0;
+    }
 
-	uint32_t _id1;
-	uint32_t _id2;
-	uint32_t _id3;
-	float _mutualInfoValue;
+    uint32_t _id1;
+    uint32_t _id2;
+    uint32_t _id3;
+    float _mutualInfoValue;
 
-	bool operator<(const MutualInfo mi) const { return _mutualInfoValue < mi._mutualInfoValue; }
+    bool operator<(const MutualInfo mi) const { return _mutualInfoValue < mi._mutualInfoValue; }
 };
-
 
 #endif // MUTUALINFO_H_
