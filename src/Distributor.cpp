0c56ebc5417fc2d3fe39e4727728ad09167af3fd
Christian Ponte <christian.ponte@udc.es>
2017-07-12 16:01:48 +0200

    Distributor: reimplemented as a static distribution

--- src/Distributor.cpp
+++ src/Distributor.cpp
@@ -4,71 +4,22 @@
 
 #include "Distributor.h"
 
-Distributor::Distributor(unsigned int proc_num, unsigned int proc_id, unsigned long item_num, bool shared,
-                         unsigned long block_size) :
-        item_num(item_num),
-        items_left(true),
-        block_size(block_size),
-        shared(shared) {
-    pthread_mutex_init(&lock, nullptr);
-
-    dist_size = item_num / proc_num +
-                (proc_id < (item_num % proc_num));
-    dist = new uint32_t[dist_size];
-    dist[0] = proc_id;
-    dist[1] = 2 * proc_num - proc_id - 1;
+Distributor::Distributor(unsigned int p_num, unsigned int p_id, unsigned long count) :
+        count(count) {
+    unsigned long dist_size = count / p_num + (p_id < (count % p_num));
+    distribution.resize(dist_size);
+    distribution[0] = p_id;
+    distribution[1] = 2 * p_num - p_id - 1;
     for (int i = 2; i < dist_size; i++) {
-        dist[i] = dist[i - 2] + 2 * proc_num;
-    }
-
-    dist_it = 0;
-    it1 = dist[dist_it++];
-    it2 = it1 + 1;
-}
-
-Distributor::~Distributor() {
-    delete[] dist;
-    pthread_mutex_destroy(&lock);
-}
-
-unsigned long Distributor::Get_pairs(std::vector<std::pair<uint32_t,uint32_t>> &values, uint64_t &totalAnal) {
-    if (shared) {
-        return Get_pairs_lock(values, totalAnal);
-    } else {
-        return Get_pairs_nolock(values, totalAnal);
+        distribution[i] = distribution[i - 2] + 2 * p_num;
     }
 }
 
-unsigned long Distributor::Get_pairs_nolock(std::vector<std::pair<uint32_t,uint32_t>> &values, uint64_t &totalAnal) {
-    unsigned long iter_block = 0;
-
-    if (items_left) {
-        while (dist_it < dist_size) {
-            while (it2 < item_num - 1 && iter_block < block_size) {
-                totalAnal += item_num - it2 - 1;
-                values.push_back(std::make_pair(it1, it2++));
-                iter_block++;
-            }
-
-            if (iter_block == block_size) {
-                return iter_block;
-            } else {
-                it1 = dist[dist_it++];
-                it2 = it1 + 1;
-            }
+void Distributor::Get_pairs(unsigned int t_num, unsigned int t_id,
+                                     std::vector<std::pair<uint32_t, uint32_t>> &values) {
+    for (long i = t_id; i < distribution.size(); i += t_num) {
+        for (uint32_t j = distribution[i] + 1; j < count - 1; j++) {
+            values.push_back(std::make_pair(distribution[i], j));
         }
-        items_left = false;
     }
-
-    return iter_block;
-}
-
-unsigned long Distributor::Get_pairs_lock(std::vector<std::pair<uint32_t,uint32_t>> &values, uint64_t &totalAnal) {
-    unsigned long iter_block = 0;
-
-    pthread_mutex_lock(&lock);
-    iter_block = Get_pairs_nolock(values, totalAnal);
-    pthread_mutex_unlock(&lock);
-
-    return iter_block;
 }
--- src/Distributor.h
+++ src/Distributor.h
@@ -6,36 +6,18 @@
 #define MPI3SNP_DISTRIBUTOR_H
 
 #include <inttypes.h>
-#include <pthread.h>
 #include <vector>
 
 class Distributor {
 public:
-    Distributor(unsigned int proc_num, unsigned int proc_id, unsigned long item_num, bool shared,
-                unsigned long block_size = DEFAULT_PAIRS_BLOCK);
+    Distributor(unsigned int p_num, unsigned int p_id, unsigned long count);
 
-    ~Distributor();
-
-    unsigned long Get_pairs(std::vector<std::pair<uint32_t,uint32_t>> &values, uint64_t &totalAnal);
-
-    static const unsigned long DEFAULT_PAIRS_BLOCK = 25000;
+    void Get_pairs(unsigned int t_num, unsigned int t_id,
+                            std::vector<std::pair<uint32_t, uint32_t>> &values);
 
 private:
-    unsigned long Get_pairs_lock(std::vector<std::pair<uint32_t,uint32_t>> &values, uint64_t &totalAnal);
-
-    unsigned long Get_pairs_nolock(std::vector<std::pair<uint32_t,uint32_t>> &values, uint64_t &totalAnal);
-
-    bool items_left;
-    unsigned long item_num;
-    unsigned long block_size;
-    bool shared;
-    pthread_mutex_t lock;
-
-    // Iterators for the SNPs
-    unsigned long dist_size, dist_it;
-    uint32_t *dist;
-    uint32_t it1, it2;
+    const unsigned long count;
+    std::vector<uint32_t> distribution;
 };
-
 
 #endif //MPI3SNP_DISTRIBUTOR_H
