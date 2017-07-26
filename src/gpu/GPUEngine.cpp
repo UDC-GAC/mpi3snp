71610b912e436cc9b34d442c5011d62803adac23
Christian Ponte <christian.ponte@udc.es>
2017-07-26 12:41:25 +0200

    GPUEngine: replaced multi-thread with multi-process implementation (wip)

--- src/gpu/GPUEngine.cpp
+++ src/gpu/GPUEngine.cpp
@@ -6,15 +6,15 @@
  */
 
 #include "GPUEngine.h"
-#include "ThreadParams.h"
 #include "EntropySearch.h"
-#include <float.h>
+#include "../Dataset.h"
+#include "../Distributor.h"
+#include <cfloat>
 #include <cstring>
 
-GPUEngine::GPUEngine(unsigned int proc_num, unsigned int proc_id, std::vector<unsigned int> gpu_ids, bool use_mi) :
+GPUEngine::GPUEngine(unsigned int proc_num, unsigned int proc_id, bool use_mi) :
         proc_num(proc_num),
         proc_id(proc_id),
-        gpu_ids(gpu_ids),
         use_mi(use_mi) {
     int avail_gpus = 0;
     if (cudaSuccess != cudaGetDeviceCount(&avail_gpus))
@@ -23,15 +23,16 @@ GPUEngine::GPUEngine(unsigned int proc_num, unsigned int proc_id, std::vector<un
         throw CUDAError("Could not find any CUDA-enabled GPU");
     }
 
-    for (unsigned int id : gpu_ids) {
-        cudaDeviceProp gpu_prop;
-        if (cudaSuccess != cudaGetDeviceProperties(&gpu_prop, id))
-            throw CUDAError(cudaGetLastError());
-        if (gpu_prop.major < 2 || !gpu_prop.canMapHostMemory) {
-            throw CUDAError("GPU " + std::to_string(id) + " does not meet compute capabilities\n" +
-                            "Name: " + gpu_prop.name + "\n" + "Compute capability: " +
-                            std::to_string(gpu_prop.major) + "." + std::to_string(gpu_prop.minor));
-        }
+    gpu_id = proc_id % avail_gpus;
+    if (cudaSuccess != cudaSetDevice(gpu_id))
+        throw CUDAError(cudaGetLastError());
+    cudaDeviceProp gpu_prop;
+    if (cudaSuccess != cudaGetDeviceProperties(&gpu_prop, gpu_id))
+        throw CUDAError(cudaGetLastError());
+    if (gpu_prop.major < 2 || !gpu_prop.canMapHostMemory) {
+        throw CUDAError("GPU " + std::to_string(gpu_id) + " does not meet compute capabilities\n" +
+                        "Name: " + gpu_prop.name + "\n" + "Compute capability: " +
+                        std::to_string(gpu_prop.major) + "." + std::to_string(gpu_prop.minor));
     }
 }
 
@@ -43,80 +44,14 @@ void GPUEngine::run(std::string tped, std::string tfam, std::vector<MutualInfo> 
     statistics.End_timer(snp_load_label);
 
     Distributor distributor(proc_num, proc_id, dataset.Get_SNP_count());
-    std::vector<pthread_t> threadIDs(gpu_ids.size(), 0);
-    std::vector<ThreadParams *> threadParams(gpu_ids.size());
-    for (int tid = 0; tid < gpu_ids.size(); tid++) {
-        // Create parameters for CPU threads
-        threadParams[tid] = new ThreadParams(gpu_ids[tid], num_outputs, dataset, use_mi, statistics);
-        distributor.Get_pairs(gpu_ids.size(), tid, threadParams[tid]->pairs);
-        // Create thread entities that call to the functions below
-        if (pthread_create(&threadIDs[tid], NULL, handle, threadParams[tid]) != 0) {
-            //Utils::exit("Thread creating failed\n");
-            // TODO: thread error handling
-            exit(0);
-        }
-    }
-
-    MutualInfo *auxMutualInfo = new MutualInfo[gpu_ids.size() * num_outputs];
-
-    // Wait for the completion of all threads
-    for (int tid = 0; tid < gpu_ids.size(); tid++) {
-        pthread_join(threadIDs[tid], NULL);
-        memcpy(&auxMutualInfo[tid * num_outputs], threadParams[tid]->mutual_info,
-               num_outputs * sizeof(MutualInfo));
-        delete threadParams[tid];
-    }
-
-    // Sort the auxiliar array and print the results
-    std::sort(auxMutualInfo, auxMutualInfo + num_outputs * gpu_ids.size());
-    mutual_info.resize(num_outputs);
-    memcpy(&mutual_info[0], auxMutualInfo + num_outputs * (gpu_ids.size() - 1), sizeof(MutualInfo) * num_outputs);
-
-    // Release the distributor
-    delete[] auxMutualInfo;
-}
-
-void *GPUEngine::handle(void *arg) {
-    ThreadParams *params = (ThreadParams *) arg;
-    Dataset &dataset = params->dataset;
-    unsigned int num_outputs = params->num_outputs;
-    unsigned int gpu_id = params->gpu_id;
-    bool isMI = params->mi;
-    Statistics &statistics = params->statistics;
-
-    if (cudaSuccess != cudaSetDevice(gpu_id))
-        throw CUDAError(cudaGetLastError());
-
-    uint32_t *hCa0, *hCa1, *hCa2, *hCt0, *hCt1, *hCt2;
-    if (cudaSuccess != cudaMallocHost(&hCa0, dataset.Get_cases()[0][0].size() * sizeof(uint32_t)))
-        throw CUDAError(cudaGetLastError());
-    memcpy(hCa0, &dataset.Get_cases()[0][0][0], dataset.Get_cases()[0][0].size() * sizeof(uint32_t));
-    if (cudaSuccess != cudaMallocHost(&hCa1, dataset.Get_cases()[0][1].size() * sizeof(uint32_t)))
-        throw CUDAError(cudaGetLastError());
-    memcpy(hCa1, &dataset.Get_cases()[0][1][0], dataset.Get_cases()[0][1].size() * sizeof(uint32_t));
-    if (cudaSuccess != cudaMallocHost(&hCa2, dataset.Get_cases()[0][2].size() * sizeof(uint32_t)))
-        throw CUDAError(cudaGetLastError());
-    memcpy(hCa2, &dataset.Get_cases()[0][2][0], dataset.Get_cases()[0][2].size() * sizeof(uint32_t));
-    if (cudaSuccess != cudaMallocHost(&hCt0, dataset.Get_ctrls()[0][0].size() * sizeof(uint32_t)))
-        throw CUDAError(cudaGetLastError());
-    memcpy(hCt0, &dataset.Get_ctrls()[0][0][0], dataset.Get_ctrls()[0][0].size() * sizeof(uint32_t));
-    if (cudaSuccess != cudaMallocHost(&hCt1, dataset.Get_ctrls()[0][1].size() * sizeof(uint32_t)))
-        throw CUDAError(cudaGetLastError());
-    memcpy(hCt1, &dataset.Get_ctrls()[0][1][0], dataset.Get_ctrls()[0][1].size() * sizeof(uint32_t));
-    if (cudaSuccess != cudaMallocHost(&hCt2, dataset.Get_ctrls()[0][2].size() * sizeof(uint32_t)))
-        throw CUDAError(cudaGetLastError());
-    memcpy(hCt2, &dataset.Get_ctrls()[0][2][0], dataset.Get_ctrls()[0][2].size() * sizeof(uint32_t));
 
     const unsigned int pairs_iter = 5000;
-    EntropySearch *search = new EntropySearch(isMI, dataset.Get_SNP_count(), dataset.Get_case_count(),
-                                              dataset.Get_ctrl_count(), num_outputs, pairs_iter,
-                                              hCa0, hCa1, hCa2, hCt0, hCt1, hCt2);
+    EntropySearch search(use_mi, dataset.Get_SNP_count(), dataset.Get_case_count(), dataset.Get_ctrl_count(),
+                         num_outputs, pairs_iter, dataset.Get_cases(), dataset.Get_ctrls());
     uint2 *auxIds;
     if (cudaSuccess != cudaMallocHost(&auxIds, pairs_iter * sizeof(uint2)))
         throw CUDAError(cudaGetLastError());
 
-    // Variables to work with the outputs
-    MutualInfo *mutualInfo = params->mutual_info;
     // The minimum value in the array
     float minMI = FLT_MAX;
     // The position of the minimum value
@@ -134,33 +69,21 @@ void *GPUEngine::handle(void *arg) {
 
     statistics.Begin_timer(timer_label);
 
-    for (unsigned long i = 0; i < params->pairs.size(); i += pairs_iter) {
-        const unsigned long num_pairs = params->pairs.size() - i < pairs_iter ? params->pairs.size() - i : pairs_iter;
+    std::vector<std::pair<uint32_t, uint32_t >> pairs;
+    distributor.Get_pairs(1, 0, pairs);
+
+    mutual_info.resize(num_outputs);
+
+    for (unsigned long i = 0; i < pairs.size(); i += pairs_iter) {
+        const unsigned long num_pairs = pairs.size() - i < pairs_iter ? pairs.size() - i : pairs_iter;
         for (int j = 0; j < num_pairs; j++) {
-            auxIds[j].x = params->pairs[i + j].first;
-            auxIds[j].y = params->pairs[i + j].second;
+            auxIds[j].x = pairs[i + j].first;
+            auxIds[j].y = pairs[i + j].second;
         }
-        search->mutualInfo(num_pairs, auxIds, mutualInfo, minMI, minMIPos, numEntriesWithMI);
+        search.mutualInfo(num_pairs, auxIds, &mutual_info.at(0), minMI, minMIPos, numEntriesWithMI);
     }
     cudaDeviceSynchronize();
 
     statistics.End_timer(timer_label);
     statistics.Add_value(analysis_label, myTotalAnal);
-
-    if (cudaSuccess != cudaFreeHost(auxIds))
-        throw CUDAError(cudaGetLastError());
-    if (cudaSuccess != cudaFreeHost(hCa0))
-        throw CUDAError(cudaGetLastError());
-    if (cudaSuccess != cudaFreeHost(hCa1))
-        throw CUDAError(cudaGetLastError());
-    if (cudaSuccess != cudaFreeHost(hCa2))
-        throw CUDAError(cudaGetLastError());
-    if (cudaSuccess != cudaFreeHost(hCt0))
-        throw CUDAError(cudaGetLastError());
-    if (cudaSuccess != cudaFreeHost(hCt1))
-        throw CUDAError(cudaGetLastError());
-    if (cudaSuccess != cudaFreeHost(hCt2))
-        throw CUDAError(cudaGetLastError());
-    delete search;
-    return nullptr;
 }
--- src/gpu/GPUEngine.h
+++ src/gpu/GPUEngine.h
@@ -13,17 +13,14 @@
 
 class GPUEngine {
 public:
-    GPUEngine(unsigned int proc_num, unsigned int proc_id, std::vector<unsigned int> gpu_ids, bool use_mi);
+    GPUEngine(unsigned int proc_num, unsigned int proc_id, bool use_mi);
 
     void run(std::string tped, std::string tfam, std::vector<MutualInfo> &mutual_info, unsigned int num_outputs,
              Statistics &statistics);
 
 private:
-    std::vector<unsigned int> gpu_ids;
-    unsigned int proc_num, proc_id;
+    unsigned int proc_num, proc_id, gpu_id;
     bool use_mi;
-
-    static void *handle(void *arg);
 };
 
 #endif /* GPUENGINE_H_ */
