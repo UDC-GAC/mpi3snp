394851e5b998c26502ef152d12966f4e1ec5744f
Christian Ponte <christian.ponte@udc.es>
2017-10-20 20:59:46 +0200

    main: moved node information printing inside condition

--- src/main.cpp
+++ src/main.cpp
@@ -23,21 +23,21 @@ int main(int argc, char **argv) {
         IOMpi::Instance().Set_print_level(IOMpi::B);
     } else if (arguments.debug) {
         IOMpi::Instance().Set_print_level(IOMpi::D);
-    }
 
-    // Print node information
-    auto info_list = Node_information::gather(0);
-    IOMpi::Instance().mprint<IOMpi::D>("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
-    for (int i = 0; i < info_list.size(); i++) {
-        IOMpi::Instance().mprint<IOMpi::D>(info_list[i]->to_string());
-        if (i == info_list.size() - 1){
-            IOMpi::Instance().mprint<IOMpi::D>("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n\n");
-        } else {
-            IOMpi::Instance().mprint<IOMpi::D>("---------------------------------------------\n");
+        // Print node information
+        auto info_list = Node_information::gather(0);
+        IOMpi::Instance().mprint<IOMpi::D>("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+        for (int i = 0; i < info_list.size(); i++) {
+            IOMpi::Instance().mprint<IOMpi::D>(info_list[i]->to_string());
+            if (i == info_list.size() - 1){
+                IOMpi::Instance().mprint<IOMpi::D>("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n\n");
+            } else {
+                IOMpi::Instance().mprint<IOMpi::D>("---------------------------------------------\n");
+            }
+            delete info_list[i];
         }
-        delete info_list[i];
+        info_list.clear();
     }
-    info_list.clear();
 
     // Execute search
     Search *search = Search::Builder::build_from_args(arguments);
